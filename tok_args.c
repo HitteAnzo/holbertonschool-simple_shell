634e533224d20dce51c47e598e5411a2d035a70b
HitteAnzo <hitteanzo@gmail.com>
2024-08-20 09:44:22 +0200

    fix realloc

--- tok_args.c
+++ tok_args.c
@@ -1,39 +1,58 @@
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * tok_args - Tokenizes a string into an array of arguments.
  * @input_buffer: The string to tokenize.
- * Return: The number of arguments filled into the array.
+ *
+ * Return: A pointer to an array of arguments.
  */
 char **tok_args(char *input_buffer)
 {
 	char *token;
-	char **argv = NULL;
-	int token_count = 0;
+	char **argv;
+	int token_count = 0, i = 0;
+	char *input_copy;
 
 	while (*input_buffer == ' ' || *input_buffer == '\t')
 		input_buffer++;
 
-	token = strtok(input_buffer, " ");
-	while (token != NULL)
-	{
-		argv = realloc(argv, (token_count + 1) * sizeof(char *));
-		if (argv == NULL)
-		{
-			perror("Memory allocation error");
-			exit(EXIT_FAILURE);
-		}
-
-		argv[token_count] = token;
-		token = strtok(NULL, " ");
-		token_count++;
-	}
-	argv = realloc(argv, (token_count + 1) * sizeof(char *));
-	if (argv == NULL)
+	input_copy = strdup(input_buffer);
+	if (input_copy == NULL)
 	{
 		perror("Memory allocation error");
 		exit(EXIT_FAILURE);
 	}
+
+	token = strtok(input_copy, " ");
+	while (token != NULL)
+	{
+		token_count++;
+		token = strtok(NULL, " ");
+	}
+
+	argv = malloc((token_count + 1) * sizeof(char *));
+	if (argv == NULL)
+	{
+		perror("Memory allocation error");
+		free(input_copy);
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(input_buffer, " ");
+	while (token != NULL)
+	{
+		argv[i++] = token;
+		token = strtok(NULL, " ");
+	}
+	argv[i] = NULL;
+
+	free(input_copy);
+
+	return (argv);
+}
 
 	argv[token_count] = NULL;
 	return (argv);
